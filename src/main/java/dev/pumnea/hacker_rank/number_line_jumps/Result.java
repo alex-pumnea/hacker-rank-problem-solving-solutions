bbfdb8effd6c1ff20e35fe0febdd5bf60be671ea
Alex Pumnea <alexandr.pumnea@gmail.com>
2023-08-27 15:37:18 +0300

    Commit message: Add 'Result.java' for number line jumps problem
    Implemented 'Result.java' for the HackerRank problem "number line jumps". The class includes a 'kangaroo' function which determines if two kangaroos starting at different positions on a number line, moving at different rates, will land at the same position at the same time.

---
+++ src/main/java/dev/pumnea/hacker_rank/number_line_jumps/Result.java
@@ -0,0 +1,34 @@
+package dev.pumnea.hacker_rank.number_line_jumps;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class Result {
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
+
+    public static void main(String[] args) {
+        int x1 = 43;
+        int v1 = 2;
+        int x2 = 70;
+        int v2 = 2;
+        log.info(kangaroo(x1, v1, x2, v2));
+    }
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        if ((x1 - x2 < 0 && v1 - v2 <= 0) || (x1 - x2) % (v1 - v2) != 0) {
+            return "NO";
+        }
+        return "YES";
+    }
+
+}
